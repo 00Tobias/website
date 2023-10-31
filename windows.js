9697bb1b296641b002892ed609d1e0546683ffc9
tobias <tobias@nights.rest>
2023-10-31 16:40:28 +0100

    Add two new windows and remove unneeded check

--- windows.js
+++ windows.js
@@ -4,18 +4,19 @@ import { createWindow } from "./window-manager.js";
 
 const windows = {
     wheel: () => createWindow("wheel", "spin 2 win", 55, 23, 35, 16),
-    readme: () => createWindow("readme", "About me", 88, 14, 88, 14),
+    readme: () => createWindow("readme", "readme", 88, 20, 38, 8),
+    console: () => createWindow("console", "console", 33, 9, 20, 4),
+    websiteSource: () => createWindow('websiteSource', 'Website Source', 100, 30, 10, 6, "<iframe src='https://git.sr.ht/~terra/website' width='100%' height='100%'></iframe>"),
 };
 
 window.windows = windows;
 
 function render() {
     const hash = window.location.hash.slice(1);
-    if (hash && !Array.from(document.getElementsByClassName("window")).some(e => e.id === hash)) {
+    if (hash) {
         windows[hash]();
     }
 }
 
 window.addEventListener("hashchange", render);
 render();
-
