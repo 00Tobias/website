797abec0ce3b5815e3d02a3777de0a280d777a3b
tobias <tobias@nights.rest>
2023-11-01 23:11:51 +0100

    readme: add some margins to the sides

--- windows.js
+++ windows.js
@@ -4,7 +4,7 @@ import { createWindow } from "./window-manager.js";
 
 const windows = {
     wheel: () => createWindow("wheel", "spin 2 win", 55, 23, 35, 16),
-    readme: () => createWindow("readme", "readme", 88, 20, 38, 8),
+    readme: () => createWindow("readme", "readme", 89, 22, 38, 8),
     console: () => createWindow("console", "console", 33, 9, 20, 4),
     websiteSource: () => createWindow('websiteSource', 'Website Source', 100, 30, 10, 6, "<iframe src='https://git.sr.ht/~terra/website' width='100%' height='100%'></iframe>"),
 };
