7dc297f5c511c2eff18bb2da5aee27ec5f494b4c
tobias <tobias@nights.rest>
2023-11-08 20:47:49 +0100

    wheel: add a clickable element to spin the wheel

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { updateTaskbar } from "./taskbar.js";
 
 // A list of window paramters to createWindow()
 const createWindowList = {
-    wheel: () => createWindow("wheel", "spin 2 win", 55, 23, 35, 16),
+    wheel: () => createWindow("wheel", "Pronoun wheel", 55, 23, 35, 16),
     readme: () => createWindow("readme", "readme", 89, 22, 38, 8),
     console: () => createWindow("console", "console", 33, 9, 20, 4),
     websiteSource: () => createWindow('websiteSource', 'Website Source', 100, 30, 10, 6, "<iframe src='https://git.sr.ht/~terra/website' width='100%' height='100%'></iframe>"),
