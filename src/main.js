9091fc359739d269e9a1ffd81ca07277dc240322
tobias <tobias@nights.rest>
2025-03-18 17:51:46 +0100

    Add many new desktop icons and two corresponding windows
    Welcome notice is now a separate window

--- src/main.js
+++ src/main.js
@@ -5,11 +5,21 @@ import { updateTaskbar } from "./taskbar.js";
 
 // A list of window paramters to createWindow()
 const createWindowList = {
-    wheel: () => createWindow("wheel", "Pronoun wheel", 55, 23, 35, 16),
-    readme: () => createWindow("readme", "readme", 89, 22, 38, 8),
-    dragonfly: () => createWindow("dragonfly", "dragonfly", 116, 38, 38, 8),
-    console: () => createWindow("console", "console", 33, 9, 20, 4),
-    injured: () => createWindow("injured", "hope this helps :D", 26, 12, 26, 12),
+    // Desktop Icons
+    welcome:   () => createWindow("welcome",   "Introduction",  58,  14, 34, 7),
+    readme:    () => createWindow("readme",    "About Me",      89,  22, 38, 8),
+    projects:  () => createWindow("projects", "Projects",      61,  9, 38, 8),
+    dragonfly: () => createWindow("dragonfly", "Dragonfly",     116, 38, 38, 8),
+    frog:      () => createWindow("frog",      "frog",          26,  12, 26, 12),
+
+    // About Me
+    console:   () => createWindow("console",   "console",       33,  9,  20, 4),
+
+    // Projects
+    nightsrest: () => createWindow('nightsrest', 'nights.rest',  100, 30, 10, 6, "<iframe src='http://0.0.0.0:8000/#welcome' width='100%' height='100%'></iframe>"),
+
+    // Unlinked
+    wheel:     () => createWindow("wheel",     "Pronoun Wheel", 55,  23, 35, 16),
 };
 window.createWindowList = createWindowList;
 
